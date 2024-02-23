d61046288b4a8df9af6cc51a20f657e73287334b
cardonandrew <cardonandrew0@gmail.com>
2024-02-23 16:04:50 -0700

    major changes

--- index.js
+++
@@ -1,9 +0,0 @@
-// import ImageCompare from "image-compare-viewer";
-// const element = document.getElementById("image-compare");
-
-// const options = {
-//   hoverStart: true,
-//   fluidMode: true,
-// };
-
-// const viewer = new ImageCompare(element, options).mount();
